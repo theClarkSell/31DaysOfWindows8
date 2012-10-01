eebd23081fa6529056a23ef380782298b34d235d
Clark Sell <clark.sell@csell.net>
2012-10-01 14:46:30 -0500

    website work

--- website/source/js/data/tiles.js
+++ website/source/js/data/tiles.js
@@ -1,7 +1,32 @@
-[
+{ "tiles": [
+    {
+        "day": "Day 3",
+        "title": "somethign",
+        "shortDesc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar odio ut turpis tempor feugiat. Ut magna nulla, pretium vitae interdum ac, porta vehicula nulla. Vestibulum faucibus, sem eget aliquet hendrerit, mauris ipsum tempus tellus, ac interdum arcu felis nec lectus. Proin diam risus, viverra ac porttitor a, tristique non quam. Duis accumsan interdum gravida. Proin nec tempus nulla. Phasellus nibh leo, posuere et facilisis non, pharetra quis odio. Nam viverra dictum enim.",
+        "publishTime": "9/26/2012 10:00AM -6:00",
+        "bottomContentImage": "/images/responsive.png",
+        "bottomContentClass": "fixed-medium",
+
+        "resources": [
+            {
+                "company": "Microsoft Corporation",
+                "webSite": "http://Microsoft.com"
+            },  
+            {   
+                "company": "Epic Systems",
+                "bio": " "
+            }
+        ]
+    },
+
     {
         "day": "Day 2",
         "title": "somethign",
+        "shortDesc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar odio ut turpis tempor feugiat. Ut magna nulla, pretium vitae interdum ac, porta vehicula nulla. Vestibulum faucibus, sem eget aliquet hendrerit, mauris ipsum tempus tellus, ac interdum arcu felis nec lectus. Proin diam risus, viverra ac porttitor a, tristique non quam. Duis accumsan interdum gravida. Proin nec tempus nulla. Phasellus nibh leo, posuere et facilisis non, pharetra quis odio. Nam viverra dictum enim.",
+        "publishTime": "9/26/2012 10:00AM -6:00",
+        "bottomContentImage": "/images/livetiles.png",
+        "bottomContentClass": "fixed-medium",
+
         "resources": [
             {
                 "company": "Microsoft Corporation",
@@ -15,7 +40,12 @@
     },
     {
         "day": "Day 1",
-        "title": "Introduction",
+        "title": "Introduction to Windows 8",
+        "shortDesc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar odio ut turpis tempor feugiat. Ut magna nulla, pretium vitae interdum ac, porta vehicula nulla. Vestibulum faucibus, sem eget aliquet hendrerit, mauris ipsum tempus tellus, ac interdum arcu felis nec lectus. Proin diam risus, viverra ac porttitor a, tristique non quam. Duis accumsan interdum gravida. Proin nec tempus nulla. Phasellus nibh leo, posuere et facilisis non, pharetra quis odio. Nam viverra dictum enim.",
+        "publishTime": "9/26/2012 10:00AM -6:00",
+        "bottomContentImage": "/images/customizability.png",
+        "bottomContentClass": "fixed-medium last",
+
         "resources": [
             {
                 "company": "Microsoft Corporation",
@@ -27,4 +57,6 @@
             }
         ]
     }
-]
+
+    ]
+}
