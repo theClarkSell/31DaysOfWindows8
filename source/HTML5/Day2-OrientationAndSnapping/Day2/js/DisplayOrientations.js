c091a26ccf2a6020e06aceb09752870a2787dd8c
Clark Sell <clark.sell@csell.net>
2012-10-15 17:05:29 -0500

    html5 rotation and snapping work

---
+++ source/HTML5/Day2-OrientationAndSnapping/Day2/js/DisplayOrientations.js
@@ -0,0 +1,43 @@
+﻿(function () {
+    "use strict";
+
+    var results;
+
+    var page = WinJS.UI.Pages.define("/pages/DisplayOrientations.html", {
+        ready: function (element, options) {
+            results = document.getElementById("resultText");
+
+            var dispProp = Windows.Graphics.Display.DisplayProperties;
+            dispProp.addEventListener("orientationchanged", updateDisplayOrientation, false);
+
+            updateDisplayOrientation(); // call to set the inital state
+        }
+    });
+
+    function updateDisplayOrientation() {
+
+        switch (Windows.Graphics.Display.DisplayProperties.currentOrientation) {
+
+            case Windows.Graphics.Display.DisplayOrientations.landscape:
+                results.innerText = "Landscape";
+                break;
+
+            case Windows.Graphics.Display.DisplayOrientations.portrait:
+                results.innerText = "Portrait";
+                break;
+
+            case Windows.Graphics.Display.DisplayOrientations.landscapeFlipped:
+                results.innerText = "Landscape (flipped)";
+                break;
+
+            case Windows.Graphics.Display.DisplayOrientations.portraitFlipped:
+                results.innerText = "Portrait (flipped)";
+                break;
+
+            default:
+                results.innerText = "Unknown";
+                break;
+        }
+    }
+
+})();
---
+++ source/HTML5/Day2-OrientationAndSnapping/Day2/js/SimpleOrientationSensor.js
@@ -0,0 +1,55 @@
+﻿(function () {
+    "use strict";
+
+    var results;
+
+    var page = WinJS.UI.Pages.define("/pages/SimpleOrientationSensor.html", {
+        ready: function (element, options) {
+           
+            results = document.getElementById("resultText");
+
+            var sensor = Windows.Devices.Sensors.SimpleOrientationSensor.getDefault();
+            if ( sensor ) {
+                sensor.addEventListener("orientationchanged", sensorOrientationChanged);
+                results.innerText = "Sensor Found";
+            }
+            else {
+                results.innerText = "No Sensor Found";
+            }
+        }
+    });
+
+    function sensorOrientationChanged(e) {
+        
+        switch (e.orientation) {
+            case Windows.Devices.Sensors.SimpleOrientation.notRotated:
+                results.innerText = "Not Rotated";
+                break;
+
+            case Windows.Devices.Sensors.SimpleOrientation.rotated90DegreesCounterclockwise:
+                results.innerText = "Rotated 90";
+                break;
+
+            case Windows.Devices.Sensors.SimpleOrientation.rotated180DegreesCounterclockwise:
+                results.innerText = "Rotated 180";
+                break;
+
+            case Windows.Devices.Sensors.SimpleOrientation.rotated270DegreesCounterclockwise:
+                results.innerText = "Rotated 270";
+                break;
+
+            case Windows.Devices.Sensors.SimpleOrientation.faceup:
+                results.innerText = "Face Up";
+                break;
+
+            case Windows.Devices.Sensors.SimpleOrientation.facedown:
+                results.innerText = "Face Down";
+                break;
+
+            default:
+                results.innerText = "Undefined orientation " + e.orientation;
+                break;
+        }
+    }
+
+})();
