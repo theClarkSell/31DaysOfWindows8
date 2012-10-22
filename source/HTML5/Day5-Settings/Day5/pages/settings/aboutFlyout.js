a5054362b95ae18efe7c0cf815b4734fb983eafb
Clark Sell <clark.sell@csell.net>
2012-10-22 10:59:55 -0500

    finished off sample for the settings flyout

---
+++ source/HTML5/Day5-Settings/Day5/pages/settings/aboutFlyout.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+    var app = WinJS.Application;
+    var ui = WinJS.UI;
+
+
+    ui.Pages.define("/pages/settings/aboutFlyout.html", {
+        ready: function (element, options) {
+            document.getElementById("about").addEventListener("afterhide", afterSettingsHide, false);
+        },
+        unload: function () {
+            document.getElementById("about").removeEventListener("afterhide", afterSettingsHide);
+        }
+    });
+    
+    function afterSettingsHide() {
+        console.log(document.getElementById("bacon").value);
+    };
+    
+})();
