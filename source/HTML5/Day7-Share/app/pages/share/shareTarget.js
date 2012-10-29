8bd59f315c84b4120cdef840ce7e4e7031a43945
Clark Sell <clark.sell@csell.net>
2012-10-29 11:02:42 -0500

    site cleanup

---
+++ source/HTML5/Day7-Share/app/pages/share/shareTarget.js
@@ -0,0 +1,51 @@
+﻿// For an introduction to the Share Contract template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232513
+//
+// TODO: Edit the manifest to enable use as a share target.  The package 
+// manifest could not be automatically updated.  Open the package manifest file
+// and ensure that support for activation of sharing is enabled.
+
+(function () {
+    "use strict";
+
+    var app = WinJS.Application;
+    var share;
+
+    function onShareSubmit() {
+        document.querySelector(".progressindicators").style.visibility = "visible";
+        document.querySelector(".commentbox").disabled = true;
+        document.querySelector(".submitbutton").disabled = true;
+
+        // TODO: Do something with the shared data stored in the 'share' var.
+
+        share.reportCompleted();
+    }
+
+    // This function responds to all application activations.
+    app.onactivated = function (args) {
+        var thumbnail;
+
+        if (args.detail.kind === Windows.ApplicationModel.Activation.ActivationKind.shareTarget) {
+            document.querySelector(".submitbutton").onclick = onShareSubmit;
+            share = args.detail.shareOperation;
+
+            document.querySelector(".shared-title").textContent = share.data.properties.title;
+            document.querySelector(".shared-description").textContent = share.data.properties.description;
+
+            thumbnail = share.data.properties.thumbnail;
+            if (thumbnail) {
+                // If the share data includes a thumbnail, display it.
+                args.setPromise(thumbnail.openReadAsync().done(function displayThumbnail(stream) {
+                    document.querySelector(".shared-thumbnail").src = window.URL.createObjectURL(stream);
+                }));
+            } else {
+                // If no thumbnail is present, expand the description  and
+                // title elements to fill the unused space.
+                document.querySelector("section[role=main] header").style.setProperty("-ms-grid-columns", "0px 0px 1fr");
+                document.querySelector(".shared-thumbnail").style.visibility = "hidden";
+            }
+        }
+    };
+
+    app.start();
+})();
--- website/source/js/data/days.js
+++ website/source/js/data/days.js
@@ -1,187 +1,100 @@
 { "days": [
     {
-        "day": "0",
-        "title": "Welcome",
+        "day": "",
+        "title": "Start",
         "shortDesc": "Lorem ipsum dolor sit amet",
         "publishTime": "9/26/2012 10:00AM -6:00",
 
         "tiles": [
-            {
-                "title": "test",
-                "textFront": "xxx",
+            {   
                 "tileSize": "largeTile-Template",
+                "title": "Start Here",
                 "image": "/images/days/0/0.png",
-                "live": "true",
-                "webSite": "xxx",
-                "frontColor": "red",
-                "backColor": "green"
+                "live": "false"
             },
             
             {
                 "tileSize": "mediumTile-Template",
+                "live": "true",
                 "title": "Clark",
                 "subtitle": "@csell5",
                 "tileImage": "images/html_csell_2.png",
-                "webSite": "http://csell.net"
+                "webSite": "http://csell.net",
+                "delay": 1500
             },
             
             {
                 "tileSize": "mediumTile-Template",
                 "title": "Jeff",
+                "live": "true",
                 "subtitle": "@jeffblankenburg",
                 "tileImage": "images/xaml_jb_2.png",
-                "webSite": "http://jeffblankenburg.com"
+                "webSite": "http://jeffblankenburg.com",
+                "delay": 2000
             },  
 
             {   
                 "tileSize": "smallTile-Template",
                 "title": "title here",
+                "live": "false",
                 "tileImage": "/images/articleHTML.png",
                 "webSite": "http://csell.net"
             },
             {   
                 "tileSize": "smallTile-Template",
                 "title": "title here",
+                "live": "false",
                 "tileImage": "/images/downloadHTML.png",
                 "webSite": "http://csell.net"
             },
             {   
                 "tileSize": "smallTile-Template",
                 "title": "title here",
+                "live": "false",
                 "tileImage": "/images/articleXAML.png",
                 "webSite": "http://csell.net"
             },
             {   
                 "tileSize": "smallTile-Template",
                 "title": "title here",
+                "live": "false",
                 "tileImage": "/images/downloadHTML.png",
                 "webSite": "http://csell.net"
             },
             {   
                 "tileSize": "smallTile-Template",
                 "title": "title here",
+                "live": "false",
                 "tileImage": "/images/articleHTML.png",
                 "webSite": "http://csell.net"
             },
             {   
                 "tileSize": "smallTile-Template",
                 "title": "title here",
+                "live": "false",
                 "tileImage": "/images/articleXAML.png",
                 "webSite": "http://csell.net"
             },
             {   
-                "tileSize": "smallTile-Template",
-                "title": "title here",
-                "tileImage": "/images/articleXAML.png",
-                "webSite": "http://csell.net"
+                "tileSize": "googleAd-template"
             },
             {
                 "tileSize": "mediumTile-Template",
                 "tileImage": "/images/ga_add.gif",
                 "title": "",
+                "live": "false",
                 "subtitle": "",
-                "webSite": "http://csell.net"
+                "webSite": "http://csell.net",
+                "delay": 2000
             },  
             {   
                 "tileSize": "smallTile-Template",
                 "title": "resources",
+                "live": "true",
                 "tileImage": "/images/more.png",
                 "webSite": "http://csell.net"
             }
         ]
-    },
-    
-    {
-      "day": "1",
-        "title": "Visual Studio 2012",
-        "shortDesc": "Lorem ipsum dolor sit amet",
-        "publishTime": "9/26/2012 10:00AM -6:00",
-
-        "tiles": [
-            {
-                "title": "test",
-                "textFront": "xxx",
-                "tileSize": "largeTile-Template",
-                "image": "/images/days/0/0.png",
-                "live": "true",
-                "webSite": "xxx",
-                "frontColor": "red",
-                "backColor": "green"
-            },
-            
-            {
-                "tileSize": "mediumTile-Template",
-                "title": "Clark",
-                "subtitle": "@csell5",
-                "tileImage": "images/html_csell_2.png",
-                "webSite": "http://csell.net"
-            },
-            
-            {
-                "tileSize": "mediumTile-Template",
-                "title": "Jeff",
-                "subtitle": "@jeffblankenburg",
-                "tileImage": "images/xaml_jb_2.png",
-                "webSite": "http://jeffblankenburg.com"
-            },  
-
-            {   
-                "tileSize": "smallTile-Template",
-                "title": "title here",
-                "tileImage": "/images/articleHTML.png",
-                "webSite": "http://csell.net"
-            },
-            {   
-                "tileSize": "smallTile-Template",
-                "title": "title here",
-                "tileImage": "/images/downloadHTML.png",
-                "webSite": "http://csell.net"
-            },
-            {   
-                "tileSize": "smallTile-Template",
-                "title": "title here",
-                "tileImage": "/images/articleXAML.png",
-                "webSite": "http://csell.net"
-            },
-            {   
-                "tileSize": "smallTile-Template",
-                "title": "title here",
-                "tileImage": "/images/downloadHTML.png",
-                "webSite": "http://csell.net"
-            },
-            {   
-                "tileSize": "smallTile-Template",
-                "title": "title here",
-                "tileImage": "/images/articleHTML.png",
-                "webSite": "http://csell.net"
-            },
-            {   
-                "tileSize": "smallTile-Template",
-                "title": "title here",
-                "tileImage": "/images/articleXAML.png",
-                "webSite": "http://csell.net"
-            },
-            {   
-                "tileSize": "smallTile-Template",
-                "title": "title here",
-                "tileImage": "/images/articleXAML.png",
-                "webSite": "http://csell.net"
-            },
-            {
-                "tileSize": "mediumTile-Template",
-                "tileImage": "/images/articleXAML.png",
-                "subtitle": "",
-                "title": "title here",
-                "webSite": "http://csell.net"
-            },  
-            {   
-                "tileSize": "smallTile-Template",
-                "title": "resources",
-                "tileImage": "/images/more.png",
-                "webSite": "http://csell.net"
-            }
-        ]   
-
     }
-    ]
+]   
 }
