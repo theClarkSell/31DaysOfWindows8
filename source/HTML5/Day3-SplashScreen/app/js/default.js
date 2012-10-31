70f62e7600ca81e1614d0a9dab70bd7fa68ad466
Clark Sell <clark.sell@csell.net>
2012-10-31 16:20:37 -0500

    finished demo for day3

--- source/HTML5/Day3-SplashScreen/app/js/default.js
+++ source/HTML5/Day3-SplashScreen/app/js/default.js
@@ -8,91 +8,47 @@
     var app = WinJS.Application;
     var activation = Windows.ApplicationModel.Activation;
 
-    var splash = null; // Variable to hold the splash screen object.
-    var dismissed = false; // Variable to track splash screen dismissal status.
-    var coordinates = { x: 0, y: 0, width: 0, height: 0 }; // Object to store splash screen image coordinates. It will be initialized during activation.
+    var _splash,
+        _dismissed = false,
+        _coordinates = { x: 0, y: 0, width: 0, height: 0 };
 
 
     app.onactivated = function (args) {
+
         if (args.detail.kind === activation.ActivationKind.launch) {
 
-            // Retrieve splash screen object
-            splash = args.detail.splashScreen;
+            _splash = args.detail.splashScreen;
+            _splash.addEventListener("dismissed", onSplashScreenDismissed, false);
 
-            // Retrieve the window coordinates of the splash screen image.
-            SdkSample.coordinates = splash.imageLocation;
+            DefaultPage.coordinates = _splash.imageLocation;
+            
+            ExtendedSplash.show(_splash);
 
-            // Register an event handler to be executed when the splash screen has been dismissed.
-            splash.addEventListener("dismissed", onSplashScreenDismissed, false);
-
-            // Create and display the extended splash screen using the splash screen object.
-            ExtendedSplash.show(splash);
-
-            // Listen for window resize events to reposition the extended splash screen image accordingly.
-            // This is important to ensure that the extended splash screen is formatted properly in response to snapping, unsnapping, rotation, etc...
             window.addEventListener("resize", onResize, false);
 
-
-
-
-            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-                // TODO: This application has been newly launched. Initialize
-                // your application here.
-            } else {
-                // TODO: This application has been reactivated from suspension.
-                // Restore application state here.
-            }
             args.setPromise(WinJS.UI.processAll());
         }
     };
 
-    app.oncheckpoint = function (args) {
-        // TODO: This application is about to be suspended. Save any state
-        // that needs to persist across suspensions here. You might use the
-        // WinJS.Application.sessionState object, which is automatically
-        // saved and restored across suspension. If you need to complete an
-        // asynchronous operation before your application is suspended, call
-        // args.setPromise().
-    };
-
-    WinJS.Navigation.addEventListener("navigated", function (eventObject) {
-        var url = eventObject.detail.location;
-        var host = document.getElementById("contentHost");
-        // Call unload method on current scenario, if there is one
-        host.winControl && host.winControl.unload && host.winControl.unload();
-        WinJS.Utilities.empty(host);
-        eventObject.detail.setPromise(WinJS.UI.Pages.render(url, host, eventObject.detail.state).then(function () {
-            WinJS.Application.sessionState.lastUrl = url;
-        }));
-    });
-
     function onSplashScreenDismissed() {
-        // Include code to be executed when the system has transitioned from the splash screen to the extended splash screen (application's first view).
-        SdkSample.dismissed = true;
+        
+        document.querySelector("#learnMore").addEventListener("click", ExtendedSplash.remove, false);
 
-        // Tear down the app's extended splash screen after completing setup operations here...
-        // In this sample, the extended splash screen is torn down when the "Learn More" button is clicked.
-        document.getElementById("learnMore").addEventListener("click", ExtendedSplash.remove, false);
-
-        // The following operation is only applicable to this sample to ensure that UI has been updated appropriately.
-        // Update scenario 1's output if scenario1.html has already been loaded before this callback executes.
-        if (document.getElementById("dismissalOutput")) {
-            document.getElementById("dismissalOutput").innerText = "Received the splash screen dismissal event.";
+        if (document.querySelector("#dismissalOutput")) {
+            document.querySelector("#dismissalOutput").innerText = "Received the splash screen dismissal event.";
         }
     }
 
     function onResize() {
-        // Safely update the extended splash screen image coordinates. This function will be fired in response to snapping, unsnapping, rotation, etc...
-        if (splash) {
-            // Update the coordinates of the splash screen image.
-            SdkSample.coordinates = splash.imageLocation;
-            ExtendedSplash.updateImageLocation(splash);
+        
+        if (_splash) {
+            DefaultPage.coordinates = _splash.imageLocation;
+            ExtendedSplash.updateImageLocation(_splash);
         }
     }
 
-    WinJS.Namespace.define("SdkSample", {
-        dismissed: dismissed,
-        coordinates: coordinates
+    WinJS.Namespace.define("DefaultPage", {
+        coordinates: _coordinates
     });
 
     app.start();
---
+++ source/HTML5/Day3-SplashScreen/app/js/extendedSplash.js
@@ -0,0 +1,48 @@
+﻿
+(function () {
+    "use strict";
+
+    function show(splash) {
+        var extendedSplashImage = document.querySelector("#extendedSplashImage");
+        position(extendedSplashImage, splash);
+
+        var extendedSplashScreen = document.querySelector("#extendedSplashScreen");
+        WinJS.Utilities.removeClass(extendedSplashScreen, "hidden");
+    }
+
+    function remove() {
+        if (isVisible()) {
+
+            var extendedSplashScreen = document.querySelector("#extendedSplashScreen")
+            WinJS.Utilities.addClass(extendedSplashScreen, "hidden");
+        }
+    }
+
+    function updateImageLocation(splash) {
+
+        if (isVisible()) {
+            var extendedSplashImage = document.querySelector("#extendedSplashImage");
+
+            position(extendedSplashImage, splash);
+        }
+    }
+
+    function position(element, splash) {
+        element.style.top = splash.imageLocation.y + "px";
+        element.style.left = splash.imageLocation.x + "px";
+        element.style.height = splash.imageLocation.height + "px";
+        element.style.width = splash.imageLocation.width + "px";
+    }
+
+    function isVisible() {
+        var extendedSplashScreen = document.querySelector("#extendedSplashScreen");
+        return !(WinJS.Utilities.hasClass(extendedSplashScreen, "hidden"));
+    }
+
+    WinJS.Namespace.define("ExtendedSplash", {
+        show: show,
+        updateImageLocation: updateImageLocation,
+        isVisible: isVisible,
+        remove: remove
+    });
+})();
