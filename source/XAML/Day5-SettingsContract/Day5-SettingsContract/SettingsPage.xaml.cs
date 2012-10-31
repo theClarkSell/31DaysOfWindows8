7ff566d9be181317ccd490847979a82410d812e2
Jeff Blankenburg <github@jeffblankenburg.com>
2012-10-31 10:12:51 -0400

    Added new Day #5 article and code (incomplete)

---
+++ source/XAML/Day5-SettingsContract/Day5-SettingsContract/SettingsPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.ApplicationSettings;
+
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Day5_SettingsContract
+{
+    public sealed partial class SettingsPage : UserControl
+    {
+        public SettingsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void MySettingsBackClicked(object sender, RoutedEventArgs e)
+        {
+            if (this.Parent.GetType() == typeof(Popup))
+            {
+                ((Popup)this.Parent).IsOpen = false;
+            }
+            SettingsPane.Show();
+
+        }
+    }
+}
