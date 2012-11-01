45aa1d883a713ce7b264589163ecf38ce99dbc5e
Clark Sell <clark.sell@csell.net>
2012-11-01 06:50:21 -0500

    working on days json file

--- website/source/js/data/days.js
+++ website/source/js/data/days.js
@@ -3,7 +3,7 @@
         "day": "0",
         "sectionLink": "#0",
         "title": "Start",
-        "publishTime": "9/26/2012 10:00",
+        "publishTime": "10/31/2012 2:08",
 
         "tiles": [
             {   
@@ -128,12 +128,126 @@
             }
         ]
     },
+
+	    {
+        "day": "2",
+        "sectionLink": "#2",
+        "title": "Snapping and Orientation",
+        "publishTime": "11/2/2012 2:08",
+
+        "tiles": [
+            {   
+                "tileSize": "largeTile-Template",
+                "title": "Start Here",
+                "image": "/images/days/1/logo.png",
+                "live": "false"
+            },
+            
+            {
+                "tileSize": "mediumTile-Template",
+                "live": "true",
+                "title": "Clark",
+                "subtitle": "@csell5",
+				"tileBack": "Read The Article",
+                "tileImage": "images/html_csell_2.png",
+                "webSite": "http://csell.net/2012/11/01/31-days-of-windows-8-day-1-the-blank-app",
+                "delay": "4000"
+            },
+            
+            {
+                "tileSize": "mediumTile-Template",
+                "title": "Jeff",
+                "live": "true",
+                "subtitle": "@jeffblankenburg",
+				"tileBack": "Read The Article",
+                "tileImage": "images/xaml_jb_2.png",
+                "webSite": "http://www.jeffblankenburg.com/2012/11/01/31-days-of-windows-8-day-1-the-blank-app",
+                "delay": "3000"
+            },  
+
+            {   
+                "tileSize": "smallTile-Template",
+                "title": "Download the HTML5 Demo",
+                "live": "false",
+                "delay": "0",
+                "tileImage": "/images/days/1/html-code.png",
+                "webSite": "https://github.com/csell5/31DaysOfWindows8/tree/master/source/HTML5/Day1-TheBlankApp"
+            },
+            {   
+                "tileSize": "smallTile-Template",
+                "title": "Download the HTML5 Demo",
+                "live": "false",
+                "delay": "0",
+                "tileImage": "/images/days/1/xaml-code.png",
+                "webSite": "http://bit.ly/31daysofwindows8-day1code"
+            },
+            {   
+                "tileSize": "smallTile-Template",
+                "title": "Getting Started",
+                "live": "true",
+                "delay": "4500",
+                "tileImage": "/images/msdn.png",
+                "webSite": "http://msdn.microsoft.com/en-us/library/windows/apps/br211386.aspx"
+            },
+            {   
+                "tileSize": "smallTile-Template",
+                "title": "Get your Developer License",
+                "live": "true",
+                "delay": "5000",
+                "tileImage": "/images/msdn.png",
+                "webSite": "http://msdn.microsoft.com/en-us/library/windows/apps/hh974578.aspx"
+            },
+            {   
+                "tileSize": "smallTile-Template",
+                "title": "Visual Studio Templates",
+                "live": "true",
+                "delay": "3500",
+                "tileImage": "/images/msdn.png",
+                "webSite": "http://msdn.microsoft.com/en-us/library/windows/apps/hh738343.aspx"
+            },
+            {   
+                "tileSize": "smallTile-Template",
+                "title": "default images",
+                "live": "true",
+                "delay": "4500",
+                "tileImage": "/images/linkXAML.png",
+                "webSite": "http://www.31daysofwindows8.com/content/defaultimages.zip"
+            },
+ 
+            {   
+                "tileSize": "smallTile-Template",
+                "title": "Signing an app package",
+                "live": "true",
+                "delay": "2500",
+                "tileImage": "/images/msdn.png",
+                "webSite": "http://msdn.microsoft.com/en-us/library/windows/apps/br230260.aspx"
+            },
+                        {
+                "tileSize": "mediumTile-Template",
+                "live": "false",
+                "title": "",
+                "subtitle": "",
+				"tileBack": "",
+                "tileImage": "/images/ga_add.gif",
+                "webSite": "http://aka.ms/cta-1",
+                "delay": "3800"
+            }, 
+            {   
+                "tileSize": "smallTile-Template",
+                "title": "resources",
+                "live": "true",
+                "delay": "4500",
+                "tileImage": "/images/more.png",
+                "webSite": "resources"
+            }
+        ]
+    },
 	
 	    {
         "day": "1",
         "sectionLink": "#1",
         "title": "The Blank Template",
-        "publishTime": "11/1/2012 2:00",
+        "publishTime": "11/1/2012 2:08",
 
         "tiles": [
             {   
