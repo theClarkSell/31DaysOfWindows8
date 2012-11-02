152911bc0bdfb2bdca01d4030df555f38206ed65
Jeff Blankenburg <github@jeffblankenburg.com>
2012-11-01 21:40:22 -0400

    Updates

---
+++ source/XAML/Day6-SearchContract/Day6-SearchContract/SearchResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+
+namespace Day6_SearchContract
+{
+    class SearchResult
+    {
+        public ImageSource Image {get; set;}
+        public string Title {get; set;}
+        public string Subtitle {get; set;}
+        public string Description {get; set;}
+    }
+}
