20a4d33280a8849c5772bacbe3aeca18ccdd129d
Jeff Blankenburg <github@jeffblankenburg.com>
2012-11-02 23:07:24 -0400

    Final Post Update for #7 XAML

--- source/XAML/Day7-ShareContract/Day7-ShareContract/MainPage.xaml.cs
+++ source/XAML/Day7-ShareContract/Day7-ShareContract/MainPage.xaml.cs
@@ -42,108 +42,115 @@ namespace Day7_ShareContract
         /// <param name="e">Event data that describes how this page was reached.  The Parameter
         /// property is typically used to configure the page.</param>
         /// 
-        ShareOperation share;
-        string title;
-        string description;
-        string text;
-        string customData;
-        string customDataFormat = "http://schema.org/People/";
-        string formattedText;
-        Uri uri;
-        IReadOnlyList<IStorageItem> storageItems;
-        IRandomAccessStreamReference bitmapImage;
-        IRandomAccessStreamReference thumbImage;
+ShareOperation share;
+string title;
+string description;
+string text;
+string customData;
+string customDataFormat = "http://schema.org/People/";
+string formattedText;
+Uri uri;
+IReadOnlyList<IStorageItem> storageItems;
+IRandomAccessStreamReference bitmapImage;
+IRandomAccessStreamReference thumbImage;
         
 
-        protected override async void OnNavigatedTo(NavigationEventArgs e)
+protected override async void OnNavigatedTo(NavigationEventArgs e)
+{
+    share = e.Parameter as ShareOperation;
+
+    await Task.Factory.StartNew(async () =>
+    {
+        title = share.Data.Properties.Title;
+        description = share.Data.Properties.Description;
+        thumbImage = share.Data.Properties.Thumbnail;
+
+        //IF THERE WAS FORMATTED TEXT SHARED
+        if (share.Data.Contains(StandardDataFormats.Html))
         {
-            share = e.Parameter as ShareOperation;
-
-            await Task.Factory.StartNew(async () =>
-            {
-                title = share.Data.Properties.Title;
-                description = share.Data.Properties.Description;
-                thumbImage = share.Data.Properties.Thumbnail;
-
-                if (share.Data.Contains(StandardDataFormats.Html))
-                {
-                    formattedText = await share.Data.GetHtmlFormatAsync();
-                }
-                if (share.Data.Contains(StandardDataFormats.Uri))
-                {
-                    uri = await share.Data.GetUriAsync();
-                }
-                if (share.Data.Contains(StandardDataFormats.Text))
-                {
-                    text = await share.Data.GetTextAsync();
-                }
-                if (share.Data.Contains(StandardDataFormats.StorageItems))
-                {
-                    storageItems = await share.Data.GetStorageItemsAsync();
-                }
-                if (share.Data.Contains(customDataFormat))
-                {
-                    customData = await share.Data.GetTextAsync(customDataFormat);
-                }
-                if (share.Data.Contains(StandardDataFormats.Bitmap))
-                {
-                    bitmapImage = await share.Data.GetBitmapAsync();
-                }
-
-                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
-                    {
-                        TitleBox.Text = title;
-                        DescriptionBox.Text = description;
-
-                        if (text != null)
-                            UnformattedTextBox.Text = text;
-                        if (uri != null)
-                        {
-                            UriButton.Content = uri.ToString();
-                            UriButton.NavigateUri = uri;
-                        }
-
-                        if (formattedText != null)
-                            HTMLTextBox.NavigateToString(HtmlFormatHelper.GetStaticFragment(formattedText));
-
-                        if (bitmapImage != null)
-                        {
-                            IRandomAccessStreamWithContentType bitmapStream = await this.bitmapImage.OpenReadAsync();
-                            BitmapImage bi = new BitmapImage();
-                            bi.SetSource(bitmapStream);
-                            WholeImage.Source = bi;
-
-                            bitmapStream = await this.thumbImage.OpenReadAsync();
-                            bi = new BitmapImage();
-                            bi.SetSource(bitmapStream);
-                            ThumbImage.Source = bi;
-                        }
-
-                        if (customData != null)
-                        {
-                            StringBuilder receivedStrings = new StringBuilder();
-                            JsonObject customObject = JsonObject.Parse(customData);
-                            if (customObject.ContainsKey("type"))
-                            {
-                                if (customObject["type"].GetString() == "http://schema.org/Person")
-                                {
-                                    receivedStrings.AppendLine("Type: " + customObject["type"].Stringify());
-                                    JsonObject properties = customObject["properties"].GetObject();
-                                    receivedStrings.AppendLine("Image: " + properties["image"].Stringify());
-                                    receivedStrings.AppendLine("Name: " + properties["name"].Stringify());
-                                    receivedStrings.AppendLine("Affiliation: " + properties["affiliation"].Stringify());
-                                    receivedStrings.AppendLine("Birth Date: " + properties["birthDate"].Stringify());
-                                    receivedStrings.AppendLine("Job Title: " + properties["jobTitle"].Stringify());
-                                    receivedStrings.AppendLine("Nationality: " + properties["Nationality"].Stringify());
-                                    receivedStrings.AppendLine("Gender: " + properties["gender"].Stringify());
-                                }
-                                CustomDataBox.Text = receivedStrings.ToString();
-                            }
-                        }
-                    });
-                });
-
+            formattedText = await share.Data.GetHtmlFormatAsync();
         }
+        //IF THERE WAS A URI SHARED
+        if (share.Data.Contains(StandardDataFormats.Uri))
+        {
+            uri = await share.Data.GetUriAsync();
+        }
+        //IF THERE WAS UNFORMATTED TEXT SHARED
+        if (share.Data.Contains(StandardDataFormats.Text))
+        {
+            text = await share.Data.GetTextAsync();
+        }
+        //IF THERE WERE FILES SHARED
+        if (share.Data.Contains(StandardDataFormats.StorageItems))
+        {
+            storageItems = await share.Data.GetStorageItemsAsync();
+        }
+        //IF THERE WAS CUSTOM DATA SHARED
+        if (share.Data.Contains(customDataFormat))
+        {
+            customData = await share.Data.GetTextAsync(customDataFormat);
+        }
+        //IF THERE WERE IMAGES SHARED.
+        if (share.Data.Contains(StandardDataFormats.Bitmap))
+        {
+            bitmapImage = await share.Data.GetBitmapAsync();
+        }
+
+        //MOVING BACK TO THE UI THREAD, THIS IS WHERE WE POPULATE OUR INTERFACE.
+        await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+            {
+                TitleBox.Text = title;
+                DescriptionBox.Text = description;
+
+                if (text != null)
+                    UnformattedTextBox.Text = text;
+                if (uri != null)
+                {
+                    UriButton.Content = uri.ToString();
+                    UriButton.NavigateUri = uri;
+                }
+
+                if (formattedText != null)
+                    HTMLTextBox.NavigateToString(HtmlFormatHelper.GetStaticFragment(formattedText));
+
+                if (bitmapImage != null)
+                {
+                    IRandomAccessStreamWithContentType bitmapStream = await this.bitmapImage.OpenReadAsync();
+                    BitmapImage bi = new BitmapImage();
+                    bi.SetSource(bitmapStream);
+                    WholeImage.Source = bi;
+
+                    bitmapStream = await this.thumbImage.OpenReadAsync();
+                    bi = new BitmapImage();
+                    bi.SetSource(bitmapStream);
+                    ThumbImage.Source = bi;
+                }
+
+                if (customData != null)
+                {
+                    StringBuilder receivedStrings = new StringBuilder();
+                    JsonObject customObject = JsonObject.Parse(customData);
+                    if (customObject.ContainsKey("type"))
+                    {
+                        if (customObject["type"].GetString() == "http://schema.org/Person")
+                        {
+                            receivedStrings.AppendLine("Type: " + customObject["type"].Stringify());
+                            JsonObject properties = customObject["properties"].GetObject();
+                            receivedStrings.AppendLine("Image: " + properties["image"].Stringify());
+                            receivedStrings.AppendLine("Name: " + properties["name"].Stringify());
+                            receivedStrings.AppendLine("Affiliation: " + properties["affiliation"].Stringify());
+                            receivedStrings.AppendLine("Birth Date: " + properties["birthDate"].Stringify());
+                            receivedStrings.AppendLine("Job Title: " + properties["jobTitle"].Stringify());
+                            receivedStrings.AppendLine("Nationality: " + properties["Nationality"].Stringify());
+                            receivedStrings.AppendLine("Gender: " + properties["gender"].Stringify());
+                        }
+                        CustomDataBox.Text = receivedStrings.ToString();
+                    }
+                }
+            });
+        });
+
+}
 
         private void PopulateScreen()
         {
