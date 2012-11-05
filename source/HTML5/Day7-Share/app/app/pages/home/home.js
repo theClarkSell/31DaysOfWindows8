8d1f067124c769882870d7a6b59f5b354dc11de9
Clark Sell <clark.sell@csell.net>
2012-11-05 15:46:53 -0600

    #7

---
+++ source/HTML5/Day7-Share/app/app/pages/home/home.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+        }
+    });
+})();
