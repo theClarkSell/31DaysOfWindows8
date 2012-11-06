22c5164cfa49aef2381b4c8d4146c33353b59082
Clark Sell <clark.sell@csell.net>
2012-11-06 07:33:02 -0600

    work on #8

--- source/HTML5/Day8-StorageAndRoaming/app/js/default.js
+++ source/HTML5/Day8-StorageAndRoaming/app/js/default.js
@@ -10,6 +10,10 @@
 
     app.onactivated = function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
+
+            //load some data from the settings
+            getLocal();
+
             if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
                 // TODO: This application has been newly launched. Initialize
                 // your application here.
@@ -22,13 +26,16 @@
     };
 
     app.oncheckpoint = function (args) {
-        // TODO: This application is about to be suspended. Save any state
-        // that needs to persist across suspensions here. You might use the
-        // WinJS.Application.sessionState object, which is automatically
-        // saved and restored across suspension. If you need to complete an
-        // asynchronous operation before your application is suspended, call
-        // args.setPromise().
+        saveLocal();
     };
+
+    function saveLocal() {
+
+    }
+
+    function getLocal() {
+
+    }
 
     app.start();
 })();
