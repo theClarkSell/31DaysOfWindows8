44a0228e512143cbd4cd89a724903715ce56c3da
Clark Sell <clark.sell@csell.net>
2012-11-06 17:29:03 -0600

    work on #8

--- source/HTML5/Day8-StorageAndRoaming/app/js/default.js
+++ source/HTML5/Day8-StorageAndRoaming/app/js/default.js
@@ -8,18 +8,26 @@
     var app = WinJS.Application;
     var activation = Windows.ApplicationModel.Activation;
 
+    var _applicationData = Windows.Storage.ApplicationData.current;
+    var _localSettings = _applicationData.roamingSettings;
+    var _tempFolder = _applicationData.temporaryFolder;
+
+    var _inputA, _labelA, _inputB, _labelB, _saveButton;
+
+    var _dataFile = {
+        firstName: "Clark",
+        lastName: "Sell",
+        address: {
+            street: "123 crazy lane",
+            city: "chicago",
+            state: "il"
+        }
+    };
+
     app.onactivated = function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
-
-            //load some data from the settings
-            getLocal();
-
             if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-                // TODO: This application has been newly launched. Initialize
-                // your application here.
             } else {
-                // TODO: This application has been reactivated from suspension.
-                // Restore application state here.
             }
             args.setPromise(WinJS.UI.processAll());
         }
@@ -30,12 +38,77 @@
     };
 
     function saveLocal() {
+        _localSettings.values["inputA"] = _inputA.value;
+        _localSettings.values["inputB"] = _inputB.value;
 
+        writeFile();
     }
 
     function getLocal() {
+        if (_localSettings.values.size > 0) {
+            _inputA.value = _localSettings.values["inputA"];
+            _inputB.value = _localSettings.values["inputB"];
+        }
+    }
+
+    function getDomElements() {
+        _inputA = document.querySelector("#inputA");
+        _labelA = document.querySelector("#labelA");
+        _inputB = document.querySelector("#inputB");
+        _labelB = document.querySelector("#labelB");
+
+        _saveButton = document.querySelector("#saveButton");
 
     }
 
+    function wireUpHandlers() {
+        _inputA.addEventListener("change", inputALeave);
+        _inputB.addEventListener("change", inputBLeave);
+        _saveButton.addEventListener("click", saveLocal);
+
+        WinJS.Application.addEventListener("forceA", inputALeave, false);
+        WinJS.Application.addEventListener("forceB", inputBLeave, false);
+
+        _applicationData.addEventListener("datachanged", datachangeHandler);
+    }
+
+    function inputALeave() {
+        _labelA.innerText+= _inputA.value;
+    }
+
+    function inputBLeave() {
+        _labelB.innerText += _inputB.value;
+    }
+
+    function datachangeHandler(eventArgs) {
+      getLocal();
+
+        WinJS.Application.queueEvent({ type: "forceA" });
+        WinJS.Application.queueEvent({ type: "forceB" });
+    }
+
+    function writeFile() {
+        _tempFolder.createFileAsync("31DaysOfWindows8.txt",
+            Windows.Storage.CreationCollisionOption.replaceExisting)
+        .then(function (sampleFile) {
+            var contents = JSON.stringify(_dataFile);
+
+            return Windows.Storage.FileIO.writeTextAsync(sampleFile, contents);
+        }).done(function () {
+      });
+    }
+
+    function readFile() {
+
+    }
+
+
+    app.onready = function () {
+        getDomElements();
+        wireUpHandlers();
+        getLocal();
+    }
+
     app.start();
+
 })();
