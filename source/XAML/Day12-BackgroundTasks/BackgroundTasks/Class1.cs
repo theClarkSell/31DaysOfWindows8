57dc90f13e0fc995d454a53345812dec8ca09659
Jeff Blankenburg <github@jeffblankenburg.com>
2012-11-07 14:37:08 -0500

    Added Day #12 project starter.  And Tile.pdf

---
+++ source/XAML/Day12-BackgroundTasks/BackgroundTasks/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace BackgroundTasks
+{
+    public class InternetChecker: IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            
+        }
+    }
+}
