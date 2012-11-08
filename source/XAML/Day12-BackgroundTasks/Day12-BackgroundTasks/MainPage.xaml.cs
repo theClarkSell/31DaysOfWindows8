6a2e56022f6383a08df4abd8d7e65780f126520b
Jeff Blankenburg <github@jeffblankenburg.com>
2012-11-08 09:15:07 -0500

    Day #12 Article and Code complete

--- source/XAML/Day12-BackgroundTasks/Day12-BackgroundTasks/MainPage.xaml.cs
+++ source/XAML/Day12-BackgroundTasks/Day12-BackgroundTasks/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.Background;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -26,13 +27,82 @@ namespace Day12_BackgroundTasks
             this.InitializeComponent();
         }
 
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.  The Parameter
-        /// property is typically used to configure the page.</param>
+        bool isTaskRegistered = false;
+
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            CheckTaskRegistration();
+        }
+
+        private void CheckTaskRegistration()
+        {
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+            {
+                if (task.Value.Name == "TileUpdater")
+                {
+                    isTaskRegistered = true;
+                    break;
+                }
+            }
+
+            if (isTaskRegistered)
+            {
+                RegisterButton.Content = "Unregister Background Task";
+            }
+            else if (!isTaskRegistered)
+            {
+                RegisterButton.Content = "Register Background Task";
+            }
+        }
+
+        private void RegisterBackgroundTask(string name, string entrypoint)
+        {
+            BackgroundTaskBuilder btb = new BackgroundTaskBuilder();
+            btb.Name = name;
+            btb.TaskEntryPoint = entrypoint;
+            btb.SetTrigger(new SystemTrigger(SystemTriggerType.InternetAvailable, false));
+
+            BackgroundTaskRegistration task = btb.Register();
+
+            task.Progress += task_Progress;
+            task.Completed += task_Completed;
+        }
+
+        void task_Completed(BackgroundTaskRegistration sender, BackgroundTaskCompletedEventArgs args)
+        {
+            //IMPLEMENT SOME CODE TO RUN AFTER YOUR BACKGROUND AGENT COMPLETES.
+        }
+
+        void task_Progress(BackgroundTaskRegistration sender, BackgroundTaskProgressEventArgs args)
+        {
+            
+        }
+
+        private void UnregisterBackgroundTask(string name)
+        {
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+            {
+                if (task.Value.Name == name)
+                {
+                    task.Value.Unregister(true);
+                }
+            }
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isTaskRegistered)
+            {
+                UnregisterBackgroundTask("TileUpdater");
+                isTaskRegistered = false;
+
+            }
+            else
+            {
+                RegisterBackgroundTask("TileUpdater", "BackgroundTasks.TileUpdater");
+            }
+
+            CheckTaskRegistration();
         }
     }
 }
