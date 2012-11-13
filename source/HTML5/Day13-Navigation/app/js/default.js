51f440d612bba49466c47eac7719bde1a39446bb
Clark Sell <clark.sell@csell.net>
2012-11-12 18:09:46 -0600

    working on #13

--- source/HTML5/Day13-Navigation/app/js/default.js
+++ source/HTML5/Day13-Navigation/app/js/default.js
@@ -7,8 +7,9 @@
 
     var app = WinJS.Application;
     var activation = Windows.ApplicationModel.Activation;
+    var nav = WinJS.Navigation;
 
-    app.onactivated = function (args) {
+    app.addEventListener("activated", function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
             if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
                 // TODO: This application has been newly launched. Initialize
@@ -17,18 +18,29 @@
                 // TODO: This application has been reactivated from suspension.
                 // Restore application state here.
             }
-            args.setPromise(WinJS.UI.processAll());
+
+            if (app.sessionState.history) {
+                nav.history = app.sessionState.history;
+            }
+            args.setPromise(WinJS.UI.processAll().then(function () {
+                if (nav.location) {
+                    nav.history.current.initialPlaceholder = true;
+                    return nav.navigate(nav.location, nav.state);
+                } else {
+                    return nav.navigate(Application.navigator.home);
+                }
+            }));
         }
-    };
+    });
 
     app.oncheckpoint = function (args) {
         // TODO: This application is about to be suspended. Save any state
-        // that needs to persist across suspensions here. You might use the
-        // WinJS.Application.sessionState object, which is automatically
-        // saved and restored across suspension. If you need to complete an
-        // asynchronous operation before your application is suspended, call
-        // args.setPromise().
+        // that needs to persist across suspensions here. If you need to 
+        // complete an asynchronous operation before your application is 
+        // suspended, call args.setPromise().
+        app.sessionState.history = nav.history;
     };
 
     app.start();
+
 })();
---
+++ source/HTML5/Day13-Navigation/app/js/scratch.js
@@ -0,0 +1,6 @@
+﻿
+
+<div id="contenthost" 
+        data-win-control="Application.PageControlNavigator" 
+        data-win-options="{home: '/pages/home/home.html'}">
+</div>
