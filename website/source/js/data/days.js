a423af8770faeb4594fb81ada1d3b9fb86e883ad
Clark Sell <clark.sell@csell.net>
2012-11-14 10:30:51 -0600

    fixed bug on website with the days page

--- website/source/js/data/days.js
+++ website/source/js/data/days.js
@@ -131,7 +131,117 @@
         ]
     },
 	
-		{
+	{
+        "day": "16",
+        "sectionLink": "#16",
+        "title": "Context Menus",
+        "publishTime": "11/16/2012 2:08",
+
+        "tiles": [
+            {   
+                "tileSize": "largeTile-Template",
+                "title": "16",
+				"subTitle": "",
+                "image": "/images/days/16/logo.png",
+                "live": "false"
+            },         
+            {
+                "tileSize": "mediumTile-Template",
+                "live": "true",
+                "title": "Clark",
+                "subtitle": "@csell5",
+				"tileBack": "Read The Article",
+                "tileImage": "images/html_csell_2.png",
+                "webSite": "http://csell.net/2012/11/15/31-days-of-windows-8-day-15-the-on-screen-keyboard",
+                "delay": "4000"
+            },            
+            {
+                "tileSize": "mediumTile-Template",
+                "title": "Jeff",
+                "live": "true",
+                "subtitle": "@jeffblankenburg",
+				"tileBack": "Read The Article",
+                "tileImage": "images/xaml_jb_2.png",
+                "webSite": "http://www.jeffblankenburg.com/2012/11/15/31-days-of-windows-8-day-15-the-on-screen-keyboard",
+                "delay": "3000"
+            },
+            {   
+                "tileSize": "smallTile-Template",
+                "title": "HTML5 Source Demo",
+                "live": "false",
+                "delay": "0",
+                "tileImage": "/images/githubhtml.png",
+                "webSite": "https://github.com/csell5/31DaysOfWindows8/tree/master/source/HTML5/Day15-OnScreenKeyboard"
+            },
+            {   
+                "tileSize": "smallTile-Template",
+                "title": "XAML Source Demo",
+                "live": "false",
+                "delay": "0",
+                "tileImage": "/images/githubxaml.png",
+                "webSite": "https://github.com/csell5/31DaysOfWindows8/tree/master/source/XAML/Day15-OnScreenKeyboard"
+            },
+            {   
+                "tileSize": "smallTile-Template",
+                "title": "XX",
+                "live": "true",
+                "delay": "4500",
+                "tileImage": "/images/articlehtml.png",
+                "webSite": "XX"
+            },
+			{   
+                "tileSize": "smallTile-Template",
+                "title": "XX",
+                "live": "true",
+                "delay": "3500",
+                "tileImage": "/images/win8sdk.png",
+                "webSite": "XX"
+            },
+            {   
+                "tileSize": "smallTile-Template",
+                "title": "XX",
+                "live": "true",
+                "delay": "5000",
+                "tileImage": "/images/tools.png",
+                "webSite": "XX"
+            },
+            {   
+                "tileSize": "smallTile-Template",
+                "title": "XX",
+                "live": "true",
+                "delay": "3500",
+                "tileImage": "/images/linkhtml.png",
+                "webSite": "XX"
+            },
+            {   
+                "tileSize": "smallTile-Template",
+                "title": "XX",
+                "live": "true",
+                "delay": "4500",
+                "tileImage": "/images/linkxaml.png",
+                "webSite": "XX"
+            },
+			{
+                "tileSize": "smallTile-Template",
+                "title": "XX",
+                "live": "true",
+                "delay": "2500",
+                "tileImage": "/images/guidelines.png",
+                "webSite": "XX"
+            },
+			
+            {   
+                "tileSize": "smallTile-Template",
+                "title": "more resources",
+                "live": "true",
+                "delay": "4500",
+                "tileImage": "/images/more.png",
+                "webSite": "resources"
+            }
+        ]
+    },
+	
+	{
         "day": "15",
         "sectionLink": "#15",
         "title": "On-Screen Keyboard",
