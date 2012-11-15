331ddfd1f0a7c07e7746f0eb1318db54ae1c024e
Clark Sell <clark.sell@csell.net>
2012-11-15 16:31:54 -0600

    finished #17

--- source/HTML5/Day17-Clipboard/app/js/default.js
+++ source/HTML5/Day17-Clipboard/app/js/default.js
@@ -8,27 +8,129 @@
     var app = WinJS.Application;
     var activation = Windows.ApplicationModel.Activation;
 
-    app.onactivated = function (args) {
-        if (args.detail.kind === activation.ActivationKind.launch) {
-            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-                // TODO: This application has been newly launched. Initialize
-                // your application here.
-            } else {
-                // TODO: This application has been reactivated from suspension.
-                // Restore application state here.
-            }
-            args.setPromise(WinJS.UI.processAll());
-        }
-    };
+    var _btnCopyText, _btnCopyHtml, _btnCopyImage, _btnCopyFile, _btnPaste,
+        _clipboard = Windows.ApplicationModel.DataTransfer.Clipboard,
+        _dataTransfer = Windows.ApplicationModel.DataTransfer,
+        _storageFile = Windows.Storage.StorageFile;
 
-    app.oncheckpoint = function (args) {
-        // TODO: This application is about to be suspended. Save any state
-        // that needs to persist across suspensions here. You might use the
-        // WinJS.Application.sessionState object, which is automatically
-        // saved and restored across suspension. If you need to complete an
-        // asynchronous operation before your application is suspended, call
-        // args.setPromise().
-    };
+    function getDomElements() {
+        _btnCopyText = document.querySelector("#btnCopyText");
+        _btnCopyHtml = document.querySelector("#btnCopyHtml");
+        _btnCopyImage = document.querySelector("#btnCopyImage");
+        _btnCopyFile = document.querySelector("#btnCopyFile");
+        _btnPaste = document.querySelector("#btnPaste");
+    }
+
+    function wireHandlers() {
+        _btnCopyText.addEventListener("click", copyTextToClipboard, false);
+        _btnCopyHtml.addEventListener("click", copyHtmlToClipboard, false);
+        _btnCopyImage.addEventListener("click", copyImageToClipboard, false);
+        _btnCopyFile.addEventListener("click", copyFileToClipboard, false);
+        _btnPaste.addEventListener("click", getClipboardContent, false);
+    }
+
+    function copyTextToClipboard() {
+        var selectedText = window.getSelection();
+
+        var dataPackage = new _dataTransfer.DataPackage();
+        dataPackage.setText(selectedText);
+
+        _clipboard.setContent(dataPackage);
+    }
+
+    function copyHtmlToClipboard() {
+        var htmlContent = document.querySelector("#content").innerHTML;
+        var htmlContentFormated = _dataTransfer.HtmlFormatHelper.createHtmlFormat(htmlContent);
+
+        var dataPackage = new _dataTransfer.DataPackage();
+        dataPackage.setHtmlFormat(htmlContentFormated);
+        dataPackage.setText(htmlContent);
+
+        _clipboard.setContent(dataPackage);
+    }
+
+    function copyImageToClipboard() {
+
+        var imageSrc = document.getElementById("myImage").src;
+        var imageUri = new Windows.Foundation.Uri(imageSrc);
+        var streamRef = Windows.Storage.Streams.RandomAccessStreamReference.createFromUri(imageUri);
+        
+        var dataPackage = new _dataTransfer.DataPackage();
+        dataPackage.setStorageItems(streamRef);
+
+        _clipboard.setContent(dataPackage);
+    }
+
+    function copyFileToClipboard () {
+        var imageSrc = document.getElementById("myImage").src;
+        var imageUri = new Windows.Foundation.Uri(imageSrc);
+
+        var splashScreenSource = document.getElementById("splashScreen").src;
+        var splashUri = new Windows.Foundation.Uri(splashScreenSource);
+        
+        var files = [];
+        var promises = [];
+
+        promises.push(_storageFile.getFileFromApplicationUriAsync(imageUri)
+            .then(function (file) {
+                files.push(file);
+        }));
+
+        promises.push(_storageFile.getFileFromApplicationUriAsync(splashUri)
+                .then(function (file) {
+                    files.push(file);
+        }));
+
+        WinJS.Promise.join(promises)
+            .then(function () {
+                var dataPackage = new _dataTransfer.DataPackage();
+                dataPackage.setStorageItems(files);
+                _clipboard.setContent(dataPackage);
+        });
+        
+        /* single file
+        storageFile.getFileFromApplicationUriAsync(imageUri)
+            .done(function (file) {
+                var dataPackage = new _dataTransfer.DataPackage();
+                dataPackage.setStorageItems([file]);
+                _clipboard.setContent(dataPackage);
+            });
+        */
+        
+    }
+
+    function getFile(uri) {
+        return new WinJS.Promise(function (complete, error, progress) {
+            storageFile.getFileFromApplicationUriAsync(uri).done(file)
+        });
+    }
+
+    function getClipboardContent() {
+
+        var dataPackage = _clipboard.getContent(),
+            sdFormats = _dataTransfer.StandardDataFormats;
+
+        if (dataPackage.contains(sdFormats.text)) {
+            dataPackage.getTextAsync().then(function (text) {
+                document.querySelector("#pasteResults").innerText = text;
+            });
+        }
+
+        if (dataPackage.contains(sdFormats.html)) {
+        }
+
+        if (dataPackage.contains(sdFormats.bitmap)) {
+        }
+
+        if (dataPackage.contains(sdFormats.storageItems)) {
+        }
+    }
+
+
+    app.onloaded = function () {
+        getDomElements();
+        wireHandlers();
+    }
 
     app.start();
 })();
