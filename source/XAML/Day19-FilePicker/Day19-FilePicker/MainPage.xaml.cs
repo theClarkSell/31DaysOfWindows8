f937549f9e8edf9ff919936e162477cafd8b2e5d
Jeff Blankenburg <github@jeffblankenburg.com>
2012-11-16 16:18:01 -0500

    Day #19 Article and Code Complete

---
+++ source/XAML/Day19-FilePicker/Day19-FilePicker/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.AccessCache;
+using Windows.Storage.Pickers;
+using Windows.Storage.Provider;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Day19_FilePicker
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private async void SingleFilePicker_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker picker = new FileOpenPicker();
+            picker.FileTypeFilter.Add(".xls");
+            StorageFile file = await picker.PickSingleFileAsync();
+        }
+
+        private async void SingleFilePickerWithDestination_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker picker = new FileOpenPicker();
+            picker.FileTypeFilter.Add(".png");
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".gif");
+            picker.FileTypeFilter.Add(".bmp");
+            picker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            picker.ViewMode = PickerViewMode.List;
+            StorageFile file = await picker.PickSingleFileAsync();
+        }
+
+        private async void MultipleFilePicker_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker picker = new FileOpenPicker();
+            picker.FileTypeFilter.Add(".png");
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".gif");
+            picker.FileTypeFilter.Add(".bmp");
+            picker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            picker.ViewMode = PickerViewMode.Thumbnail;
+            IReadOnlyList<StorageFile> files = await picker.PickMultipleFilesAsync();
+        }
+
+        private async void SaveAFile_Click(object sender, RoutedEventArgs e)
+        {
+            FileSavePicker saver = new FileSavePicker();
+            saver.SuggestedStartLocation = PickerLocationId.Desktop;
+            saver.FileTypeChoices.Add("Text File", new List<string>() { ".txt" });
+            saver.FileTypeChoices.Add("Microsoft Excel", new List<string>() { ".xls" });
+            saver.FileTypeChoices.Add("Image", new List<string>() { ".png", ".jpg", ".bmp" });
+            saver.SuggestedFileName = "PeriodicTableOfTheElements";
+            StorageFile file = await saver.PickSaveFileAsync();
+
+            if (file != null)
+            {
+                CachedFileManager.DeferUpdates(file);
+                await FileIO.WriteTextAsync(file, "This is a link to the Periodic Table of the Elements.  http://www.ptable.com/  You didn't expect to find all of the contents here, did you?");
+                FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(file);
+            }
+        }
+
+        private async void SelectFolder_Click(object sender, RoutedEventArgs e)
+        {
+            FolderPicker picker = new FolderPicker();
+            picker.FileTypeFilter.Add(".xls");
+            StorageFolder folder = await picker.PickSingleFolderAsync();
+            if (folder != null)
+            {
+                StorageApplicationPermissions.FutureAccessList.AddOrReplace("DefaultFolder", folder);
+            }
+        }
+    }
+}
