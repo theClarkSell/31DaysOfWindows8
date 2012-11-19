d998348685858b5a2457d41afe7b130c218f16d9
Clark Sell <clark.sell@csell.net>
2012-11-19 15:56:09 -0600

    working on #21

--- source/HTML5/Day21-CameraCapture/app/js/default.js
+++ source/HTML5/Day21-CameraCapture/app/js/default.js
@@ -10,25 +10,37 @@
 
     app.onactivated = function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
-            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-                // TODO: This application has been newly launched. Initialize
-                // your application here.
-            } else {
-                // TODO: This application has been reactivated from suspension.
-                // Restore application state here.
-            }
             args.setPromise(WinJS.UI.processAll());
         }
     };
 
-    app.oncheckpoint = function (args) {
-        // TODO: This application is about to be suspended. Save any state
-        // that needs to persist across suspensions here. You might use the
-        // WinJS.Application.sessionState object, which is automatically
-        // saved and restored across suspension. If you need to complete an
-        // asynchronous operation before your application is suspended, call
-        // args.setPromise().
-    };
+    var _imageCapture, _result;
+
+    function getDomElements() {
+        _imageCapture = document.querySelector("#imageCapture");
+        _result = document.querySelector("#result");
+    }
+    
+    function wireEventHandlers() {
+        _imageCapture.addEventListener("click", imageCapture, false);
+    }
+
+    function imageCapture() {
+        var captureUI = new Windows.Media.Capture.CameraCaptureUI();
+        captureUI.captureFileAsync(Windows.Media.Capture.CameraCaptureUIMode.photo).then(function (capturedItem) {
+            if (capturedItem) {
+                document.getElementById("message").innerHTML = "User captured a photo."
+            }
+            else {
+                document.getElementById("message").innerHTML = "User didn't capture a photo."
+            }
+        });
+    }
+
+    app.onloaded = function () {
+        getDomElements();
+        wireEventHandlers();
+    }
 
     app.start();
 })();
