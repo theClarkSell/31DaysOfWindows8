f5bbd49038b804205fe41b5e1044cb41d99dec99
Clark Sell <clark.sell@csell.net>
2012-11-20 15:51:21 -0600

    started on #22

---
+++ source/HTML5/Day22-PlayTo/app/js/default.js
@@ -0,0 +1,32 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    var ptm = Windows.Media.PlayTo.PlayToManager.getForCurrentView();
+    ptm.addEventListener("sourcerequested", sourceRequestHandler, false);
+
+    function sourceRequestHandler(e) {
+        try {
+            
+            e.sourceRequest.setSource(document.querySelector("#audioTag").msPlayToSource);
+
+        } catch (ex) {
+            
+                
+        }
+    }
+
+    app.start();
+})();
