f6ad9d1e5bf544e056dde1591848a921222ff291
Clark Sell <clark.sell@csell.net>
2012-11-21 10:54:32 -0600

    finished source code for #23

--- source/HTML5/Day23-Compass/app/js/default.js
+++ source/HTML5/Day23-Compass/app/js/default.js
@@ -10,25 +10,42 @@
 
     app.onactivated = function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
-            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-                // TODO: This application has been newly launched. Initialize
-                // your application here.
-            } else {
-                // TODO: This application has been reactivated from suspension.
-                // Restore application state here.
-            }
             args.setPromise(WinJS.UI.processAll());
         }
     };
 
-    app.oncheckpoint = function (args) {
-        // TODO: This application is about to be suspended. Save any state
-        // that needs to persist across suspensions here. You might use the
-        // WinJS.Application.sessionState object, which is automatically
-        // saved and restored across suspension. If you need to complete an
-        // asynchronous operation before your application is suspended, call
-        // args.setPromise().
-    };
+    var _trueNorth, _magNorth;
+
+    function onReadingChanged(e) {
+        var newReading = e.reading;
+        _magNorth.innerText = newReading.headingMagneticNorth.toFixed(2);
+
+        if (newReading.headingTrueNorth != null) {
+            _trueNorth.innerText = reading.headingTrueNorth.toFixed(2);
+        }
+    }
+
+    function startCompass() {
+        var compass = Windows.Devices.Sensors.Compass.getDefault();
+
+        if (compass) {
+            var minimumReportInterval = compass.minimumReportInterval;
+            var reportInterval = minimumReportInterval > 16 ? minimumReportInterval : 16;
+            compass.reportInterval = reportInterval;
+
+            compass.addEventListener("readingchanged", onReadingChanged);
+        }
+    }
+
+    function getDomElements() {
+        _trueNorth = document.querySelector("#trueNorth");
+        _magNorth = document.querySelector("#magNorth");
+    }
+
+    app.onloaded = function () {
+        getDomElements();
+        startCompass();
+    }
 
     app.start();
 })();
