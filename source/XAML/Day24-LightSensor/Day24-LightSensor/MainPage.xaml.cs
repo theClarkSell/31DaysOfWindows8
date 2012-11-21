d6b89e47621247ecb40987a3f2786a3a3027583c
Jeff Blankenburg <github@jeffblankenburg.com>
2012-11-21 10:57:40 -0500

    Day #24 Almost Done

---
+++ source/XAML/Day24-LightSensor/Day24-LightSensor/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Windows.Devices.Sensors;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Day24_LightSensor
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        LightSensor sensor;
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            sensor = LightSensor.GetDefault();
+            if (sensor != null)
+            {
+                sensor.ReadingChanged += sensor_ReadingChanged;
+                Data.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                NoSensorMessage.Visibility = Visibility.Visible;
+            }
+        }
+
+        async void sensor_ReadingChanged(LightSensor sender, LightSensorReadingChangedEventArgs args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                Lux.Text = args.Reading.IlluminanceInLux.ToString();
+                TimeStamp.Text = args.Reading.Timestamp.ToString();
+            });
+        }
+    }
+}
