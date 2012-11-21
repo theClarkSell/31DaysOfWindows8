814c9f60f4439c90fa515c4d5b632e3473ecdbe9
Clark Sell <clark.sell@csell.net>
2012-11-21 12:32:10 -0600

    inital add of #24

---
+++ source/HTML5/Day24-LightSensor/app/js/default.js
@@ -0,0 +1,32 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+
+    function getDomElements() {
+
+    }
+
+    function wireEventHandlers() {
+
+    }
+
+    app.onready = function () {
+        getDomElements();
+        wireEventHandlers();
+    }
+
+    app.start();
+})();
