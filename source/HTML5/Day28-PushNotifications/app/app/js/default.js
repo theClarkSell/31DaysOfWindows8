b483365b1798c1dd93e761b3a4059cc9c0a0fc74
Clark Sell <clark.sell@csell.net>
2012-11-26 17:03:43 -0600

    inital work on 28

---
+++ source/HTML5/Day28-PushNotifications/app/app/js/default.js
@@ -0,0 +1,65 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    var notify = Windows.UI.Notifications;
+    var push = Windows.Networking.PushNotifications;
+    var net = Windows.Networking.Connectivity;
+    var wsc = Windows.Security.Cryptography;
+    var popups = Windows.UI.Popups;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize
+                // your application here.
+            } else {
+                // TODO: This application has been reactivated from suspension.
+                // Restore application state here.
+            }
+
+            notify.TileUpdateManager.createTileUpdaterForApplication().clear();
+            notify.BadgeUpdateManager.createBadgeUpdaterForApplication().clear();
+
+            var profile = net.NetworkInformation.getInternetConnectionProfile();
+
+            if (profile.getNetworkConnectivityLevel() === net.NetworkConnectivityLevel.internetAccess) {
+                push.PushNotificationChannelManager.createPushNotificationChannelForApplicationAsync().then(function (channel) {
+
+                    var postData = { uri: channel.uri };
+
+                    WinJS.xhr({
+                        type: "POST",
+                        headers: { "Content-Type": "application/json; charset=utf-8" },
+                        url: "http://31daysofwindows8.com/api/PushNotifications",
+                        data: JSON.stringify(postData)
+                    }).then(function (xhr) {
+                        if (xhr.status < 200 || xhr.status >= 300) {
+                            var dialog = new popups.MessageDialog("Unable to open push notification channel");
+                            dialog.showAsync();
+                        }
+                    });
+                });
+            }
+
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. You might use the
+        // WinJS.Application.sessionState object, which is automatically
+        // saved and restored across suspension. If you need to complete an
+        // asynchronous operation before your application is suspended, call
+        // args.setPromise().
+    };
+
+    app.start();
+})();
