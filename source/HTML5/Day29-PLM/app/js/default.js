73e5769f2b1886fb27f9d6175c370d1a25a58c85
Clark Sell <clark.sell@csell.net>
2012-11-28 15:05:52 -0600

    saving

--- source/HTML5/Day29-PLM/app/js/default.js
+++ source/HTML5/Day29-PLM/app/js/default.js
@@ -7,6 +7,13 @@
 
     var app = WinJS.Application;
     var activation = Windows.ApplicationModel.Activation;
+    var webUI = Windows.UI.WebUI.WebUIApplication;
+
+    webUI.addEventListener("resuming", onResume, false);
+
+    function onResume() {
+        WinJS.log && WinJS.log("app resuming", "31 days", "status");
+    }
 
     app.onactivated = function (args) {
         WinJS.log && WinJS.log("app.onactivated", "31 days", "status");
@@ -42,15 +49,7 @@
     };
 
     app.oncheckpoint = function (args) {
-        // TODO: This application is about to be suspended. Save any state
-        // that needs to persist across suspensions here. You might use the
-        // WinJS.Application.sessionState object, which is automatically
-        // saved and restored across suspension. If you need to complete an
-        // asynchronous operation before your application is suspended, call
-        // args.setPromise().
-
         WinJS.log && WinJS.log("app.oncheckpoint called.", "31 days", "status");
-
     };
 
 
