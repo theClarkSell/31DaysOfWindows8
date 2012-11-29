5d9b78f8f72b768e69ee2bbae5578789550b114b
Clark Sell <clark.sell@csell.net>
2012-11-29 11:57:44 -0600

    published #29

---
+++ source/HTML5/Day29-PLM/app/js/scratch.js
@@ -0,0 +1,36 @@
+﻿
+
+var app = WinJS.Application;
+
+app.onactivated = function (args) {
+    WinJS.log && WinJS.log("app.onactivated", "31 days", "status");
+
+    if (args.detail.kind === activation.ActivationKind.launch) {
+        WinJS.log && WinJS.log("Activation Kind === Launch.", "31 days", "status");
+
+        switch (args.detail.previousExecutionState) {
+
+            case activation.ApplicationExecutionState.terminated:
+                WinJS.log && WinJS.log("previousExecutionState terminated", "31 days", "status");
+                break;
+
+            case activation.ApplicationExecutionState.running:
+                WinJS.log && WinJS.log("previousExecutionState running", "31 days", "status");
+                break;
+
+            case activation.ApplicationExecutionState.suspended:
+                WinJS.log && WinJS.log("previousExecutionState suspended", "31 days", "status");
+                break;
+
+            case activation.ApplicationExecutionState.closedByUser:
+                WinJS.log && WinJS.log("previousExecutionState closedByUser", "31 days", "status");
+                break;
+
+            case activation.ApplicationExecutionState.notRunning:
+                WinJS.log && WinJS.log("previousExecutionState notRunning", "31 days", "status");
+                break;
+        }
+
+        args.setPromise(WinJS.UI.processAll());
+    }
+};
